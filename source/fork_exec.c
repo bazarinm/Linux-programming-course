943b0e17b030512135d2ca5b08ed6c6b22b7933b
Mikhail <32748867+bazarinm@users.noreply.github.com>
2018-05-24 23:35:09 +0300

    Update fork_exec.c

--- source/fork_exec.c
+++ source/fork_exec.c
@@ -8,13 +8,13 @@ int main(int argc, char *argv[], char *env[])
 	pid_t ch_pid;
 	if((ch_pid = fork()) == 0) //child process
 	{
-	    char *exec_argv[] = {"id", NULL};
-	    printf("Hello from child, my pid is %i\n", getpid());
-	    execve("/usr/bin/id", exec_argv, env); //execute a program
+	 	char *exec_argv[] = {"id", NULL};
+		printf("Hello from child, my pid is %i\n", getpid());
+		execve("/usr/bin/id", exec_argv, env); //execute a program
 	} 
 	else
 	{
-	    int ret;
+		int ret;
 		wait(&ret); //wait for child's exit status
 		printf("Hello from parent. My pid is %i, my child's pid is %i\n", getpid(), ch_pid);
 		printf("Its exit status is %i\n", WEXITSTATUS(ret));
